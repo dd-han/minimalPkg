dcef4a373bf9d1136f3afb980f78ca026c59d5bf
root <root@dd-han-synodev.dynamic-ip6.hinet.net>
2018-06-11 21:45:08 +0800

    rewrite to php restful api

--- INFO.sh
+++ INFO.sh
@@ -3,11 +3,11 @@
 
 source /pkgscripts/include/pkg_util.sh
 
-package="minimalPkg"
-version="1.0.0000"
-displayname="Minimal Package"
-maintainer="Synology Inc."
+package="dlcardPkg"
+version="1.0.0001"
+displayname="dlcard nginx config package"
+maintainer="dd-han's working"
 arch="$(pkg_get_unified_platform)"
-description="this is a minimal package"
+description="this package contain nginx config for dlcard System"
 [ "$(caller)" != "0 NULL" ] && return 0
 pkg_dump_info
